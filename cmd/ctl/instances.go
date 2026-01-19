b0e32de713fd2dd573507fe3f37c200202fa820a
faradayfan <faradayfan@protonmail.com>
2026-01-19 16:22:48 -0700

    feat(ctl): instance enable/disable

--- cmd/ctl/instances.go
+++ cmd/ctl/instances.go
@@ -14,12 +14,14 @@ func newInstancesCmd() *cobra.Command {
 		Short: "Manage server instances",
 	}
 
-	cmd.AddCommand(newInstancesListCmd())
 	cmd.AddCommand(newInstanceCreateCmd())
 	cmd.AddCommand(newInstanceDeleteCmd())
+	cmd.AddCommand(newInstanceDisableCmd())
+	cmd.AddCommand(newInstanceEnableCmd())
+	cmd.AddCommand(newInstancesListCmd())
 	cmd.AddCommand(newInstanceStartCmd())
-	cmd.AddCommand(newInstanceStopCmd())
 	cmd.AddCommand(newInstanceStatusCmd())
+	cmd.AddCommand(newInstanceStopCmd())
 
 	return cmd
 }
@@ -136,6 +138,34 @@ func newInstanceStatusCmd() *cobra.Command {
 			agentID := url.PathEscape(args[0])
 			instance := url.PathEscape(args[1])
 			return api.PrintGET(fmt.Sprintf("/agents/%s/instances/%s/status", agentID, instance))
+		},
+	}
+}
+
+func newInstanceEnableCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "enable <agentID> <instance>",
+		Short: "Enable an instance (allow starting)",
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			api := GetAPI(cmd.Context())
+			agentID := url.PathEscape(args[0])
+			instance := url.PathEscape(args[1])
+			return api.PrintPOST(fmt.Sprintf("/agents/%s/instances/%s/enable", agentID, instance), map[string]any{})
+		},
+	}
+}
+
+func newInstanceDisableCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "disable <agentID> <instance>",
+		Short: "Disable an instance (prevent starting)",
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			api := GetAPI(cmd.Context())
+			agentID := url.PathEscape(args[0])
+			instance := url.PathEscape(args[1])
+			return api.PrintPOST(fmt.Sprintf("/agents/%s/instances/%s/disable", agentID, instance), map[string]any{})
 		},
 	}
 }
